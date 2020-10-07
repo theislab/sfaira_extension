46b2b172907ffe434b61c9f89646f5789c5bde0b
le-ander <20015434+le-ander@users.noreply.github.com>
2020-10-07 14:11:44 +0200

    add missing nmf topologies

--- sfaira_extension/versions/topology_versions/human/embedding/__init__.py
+++ sfaira_extension/versions/topology_versions/human/embedding/__init__.py
@@ -1,5 +1,6 @@
 from sfaira_extension.versions.topology_versions.human.embedding.ae import AE_TOPOLOGIES
 from sfaira_extension.versions.topology_versions.human.embedding.linear import LINEAR_TOPOLOGIES
+from sfaira_extension.versions.topology_versions.human.embedding.nmf import NMF_TOPOLOGIES
 from sfaira_extension.versions.topology_versions.human.embedding.vae import VAE_TOPOLOGIES
 from sfaira_extension.versions.topology_versions.human.embedding.vaeiaf import VAEIAF_TOPOLOGIES
 from sfaira_extension.versions.topology_versions.human.embedding.vaevamp import VAEVAMP_TOPOLOGIES
