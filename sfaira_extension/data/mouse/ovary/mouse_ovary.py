8f41305fe9f610a8ac38c0372fb8cd764e94b1de
le-ander <20015434+le-ander@users.noreply.github.com>
2020-12-01 13:58:16 +0100

    rename mouse ovary organ to femalegonad

--- sfaira_extension/data/mouse/ovary/mouse_ovary.py
+++ sfaira_extension/data/mouse/ovary/mouse_ovary.py
@@ -4,7 +4,7 @@ from typing import Union
 from sfaira.data import DatasetGroupBase
 
 
-class DatasetGroupOvary(DatasetGroupBase):
+class DatasetGroupFemalegonad(DatasetGroupBase):
 
     def __init__(
         self, 
