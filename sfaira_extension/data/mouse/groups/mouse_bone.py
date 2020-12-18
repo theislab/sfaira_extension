d0829d1e42dce2bfa8bf7021ac2962b486de779b
david.seb.fischer <david.seb.fischer@gmail.com>
2020-12-18 14:59:32 +0100

    adopted new doi wise structure of data

---
+++ sfaira_extension/data/mouse/groups/mouse_bone.py
@@ -0,0 +1,18 @@
+import os
+from typing import Union
+
+from sfaira.data import DatasetGroupBase
+
+
+class DatasetGroupBone(DatasetGroupBase):
+
+    def __init__(
+        self, 
+        path: Union[str, None] = None,
+        meta_path: Union[str, None] = None
+    ):
+        super().__init__()
+        datasets = [
+        ]
+        keys = [x.id for x in datasets]
+        self.datasets = dict(zip(keys, datasets))
